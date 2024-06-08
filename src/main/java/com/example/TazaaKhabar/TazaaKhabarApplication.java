e7f530d376f8102cf46855c849137f146cf4f25a
SujitJamdade <jamdadesujit58@gmail.com>
2024-06-08 19:28:59 +0530

    remote backend API's newsapi

---
+++ src/main/java/com/example/TazaaKhabar/TazaaKhabarApplication.java
@@ -0,0 +1,13 @@
+package com.example.TazaaKhabar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazaaKhabarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TazaaKhabarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TazaaKhabar/TazaaKhabarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TazaaKhabar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TazaaKhabarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
